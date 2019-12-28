17c3ca3dee3269107c63db4045911ecbe3442298
nzw9314 <nzw18717174424@icloud.com>
2019-12-28 22:48:54 +0800

    Change

--- cookie_baidu_tieba.js
+++ cookie_baidu_tieba.js
@@ -1,24 +1,46 @@
 /*
-[rewrite_local]
-^https?:\/\/tieba.baidu\.com url script-response-body cookie_baidu_tieba.js
+TieBa Checkin Get Cookie.
 
-[mitm]
-hostname = tieba.baidu.com
+About the author:
+Telegram channel: @NobyDa
+Telegram bots: @NobyDa_bot
+
+Description :
+When TieBa app is opened, click "My", If notification gets cookie success, you can use the check in script. because script will automatically judgment whether the cookie is updated, so you dont need to disable it manually.
+
+[rewrite_local]
+https?:\/\/c\.tieba\.baidu\.com\/c\/s\/login url script-response-body TieBa_GetCookie_QX.js
+
+MITM = c.tieba.baidu.com
 */
 
 var headerCookie = $request.headers["Cookie"];
-if (headerCookie) {
-  if (headerCookie.indexOf("BDUSS") == -1) {
-    $notify("写入贴吧Cookie失败‼️‼️", "未获取到BDUSS", "请重试");
+
+if ($request.headers["Cookie"]) {
+  if ($prefs.valueForKey("CookieTB") != undefined) {
+    if ($prefs.valueForKey("CookieTB") != headerCookie) {
+      if (headerCookie.indexOf("BDUSS") == -1) {
+        $notify("更新贴吧Cookie失败‼️‼️ 无BDUSS", "", "");
+      } else {
+        var cookie = $prefs.setValueForKey(headerCookie, "CookieTB");
+        if (!cookie) {
+          $notify("更新贴吧Cookie失败‼️", "", "");
+        } else {
+          $notify("更新贴吧Cookie成功 🎉", "", "");
+        }
+      }
+    }
   } else {
-    var cookie = $prefs.setValueForKey(headerCookie, "CookieTB");
-    if (!cookie) {
-      $notify("写入贴吧Cookie失败‼️‼️", "", "请重试");
+    if (headerCookie.indexOf("BDUSS") == -1) {
+      $notify("首次写入贴吧Cookie失败,无BDUSS,请重试‼️‼️", "", "");
     } else {
-      $notify("写入贴吧Cookie成功🎉", "", "您可以手动禁用此脚本");
+      var cookie = $prefs.setValueForKey(headerCookie, "CookieTB");
+      if (!cookie) {
+        $notify("首次写入贴吧Cookie失败‼️", "", "");
+      } else {
+        $notify("首次写入贴吧Cookie成功 🎉", "", "");
+      }
     }
   }
-} else {
-  $notify("写入贴吧Cookie失败‼️‼️", "", "请退出账号, 重复步骤");
 }
-$done({});
+$done({})
