0ca15b24dcd2a104a418117d6c21cfc7c874980d
nzw9314 <nzw18717174424@icloud.com>
2020-01-13 21:44:19 +0800

    fix Changes

--- langkhach270389/memrise.vip.js
+++ langkhach270389/memrise.vip.js
@@ -1,18 +1,18 @@
 let obj = JSON.parse($response.body);
 let url = $request.url;
-const cons1 = "api.memrise.com/v1.16/me/";
-const cons2 = "api.memrise.com/v1.16/dashboard/";
-const cons3 = "leaderboards/following/";
+const cons1 = "me/";
+const cons2 = "dashboard/";
+const cons3 = "following/";
 
-if(url.indexOf(cons1) != -1) {
+if(url.endsWith(cons1)) {
 obj["profile"]["is_pro"] = true;
 }
 
-if(url.indexOf(cons2) != -1) {
+if(url.endsWith(cons2)) {
 obj["user"]["is_premium"] = true;
 }
 
-if(url.indexOf(cons3) != -1) {
+if(url.indexOf(cons3) != -1 ) {
 obj["users"][0]["is_premium"] = true;
 }
 $done({body: JSON.stringify(obj)});
