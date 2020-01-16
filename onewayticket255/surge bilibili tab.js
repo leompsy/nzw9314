ec72c4ebad9da52543122229dd576718daf0a1e0
nzw9314 <nzw18717174424@icloud.com>
2020-01-16 20:37:47 +0800

    Update

--- onewayticket255/surge bilibili tab.js
+++ onewayticket255/surge bilibili tab.js
@@ -1,19 +1,19 @@
 //Customize whitelist
-let whitelist=['追番','推荐','直播','热门','影视']
+let whitelist = ['追番', '推荐', '直播', '热门', '影视', '拜年祭']
 
 let body = $response.body
-body=JSON.parse(body)
+body = JSON.parse(body)
 
 body['data']['tab'].forEach((element, index) => {
-if(!(whitelist.includes(element['name']))) body['data']['tab'].splice(index,1)  
-});
+    if (!(whitelist.includes(element['name']))) { body['data']['tab'].splice(index, 1) }
+})
 
-body['data']['bottom'].forEach((element, index)=> {
-    if(element['pos']==4){      
-       body['data']['bottom'].splice(index,1)  
+body['data']['bottom'].forEach((element, index) => {
+    if (element['pos'] == 4) {
+        body['data']['bottom'].splice(index, 1)
     }
 })
 
 delete body['data']['top']
-body=JSON.stringify(body)
-$done({body}) 
+body = JSON.stringify(body)
+$done({ body }) 
