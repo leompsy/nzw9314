210e805a8f5a63561d5208078b2500340fc60b02
nzw9314 <52601963+nzw9314@users.noreply.github.com>
2020-01-18 19:29:05 +0800

    人人影视字幕组去广告

--- Script/YYeTs.js
+++ Script/YYeTs.js
@@ -2,6 +2,9 @@ let body = $response.body;
 body = JSON.parse(body);
 if (-1 != $request.url.indexOf('init')) {
     body['adSwitch'] = [];
+    body['adUpdateTime'] += 86400*365;
+    // 关闭弹幕功能
+    // body['barrageIp'] = "";
 }
 if (-1 != $request.url.indexOf('ads')) {
     // 留下影片推荐,防止轮播位置空白
@@ -19,8 +22,8 @@ if (-1 != $request.url.indexOf('ads')) {
 body = JSON.stringify(body);
 $done({body});
 /**
- * http-response http://ctrl.playcvn.com/app/(init|ads) requires-body=true,script-path=YYets.js
  * MITM: ctrl.playcvn.com
  * 人人影视字幕组(https://itunes.apple.com/cn/app/%E4%BA%BA%E4%BA%BA%E5%BD%B1%E8%A7%86%E5%AD%97%E5%B9%95%E7%BB%84/id1052761459)
  * 应用有广告缓存,可能需要重装
+ * http-response http://ctrl.playcvn.com/app/(init|ads) requires-body=true,script-path=YYeTs.js
  */
