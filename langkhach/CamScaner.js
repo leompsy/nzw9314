8453d7181aca7a70e9ddab43402b18d9e4bf0d91
nzw9314 <nzw18717174424@icloud.com>
2020-01-28 19:08:03 +0800

    CamScaner.js  2099

--- langkhach/CamScaner.js
+++ langkhach/CamScaner.js
@@ -1,3 +1,3 @@
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"4073275762"}}};
 $done({body: JSON.stringify(obj)});
