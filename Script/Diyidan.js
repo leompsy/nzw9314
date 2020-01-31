694b025b4a0b433bf8e641343dd229bb898535d5
nzw9314 <nzw18717174424@icloud.com>
2020-01-31 13:51:50 +0800

    add 使用说明

--- Script/Diyidan.js
+++ Script/Diyidan.js
@@ -1,3 +1,17 @@
+/*
+第一弹 去广告+原画 (By Miao Miao)
+
+Surge4:
+http-response ^https:\/\/api\.diyidan\.net\/v0\.3\/(user\/personal_homepage|vip_user\/info|tv_series\/index\?appChanne) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/Diyidan.js
+
+QuantumultX(远程):
+^https:\/\/api\.diyidan\.net\/v0\.3\/(user\/personal_homepage|vip_user\/info|tv_series\/index\?appChanne) url script-response-body https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/Diyidan.js
+
+QuantumultX(本地):
+^https:\/\/api\.diyidan\.net\/v0\.3\/(user\/personal_homepage|vip_user\/info|tv_series\/index\?appChanne) url script-response-body Diyidan.js
+
+*/
+
 let url = $request.url;
 const path1 = "user/personal_homepage";
 const path2 = "vip_user/info";
