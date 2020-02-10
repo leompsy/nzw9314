879523fb0c80a5ffd194c117b9e359b06c7c664e
nzw9314 <iwi35zw6vt@privaterelay.appleid.com>
2020-02-10 22:53:55 +0800

    更新 [每日壹句] 增加 蚂蚁森林收能量提醒

--- yichahucha/cron_daily.js
+++ yichahucha/cron_daily.js
@@ -1,20 +1,20 @@
 /*
 README：https://github.com/yichahucha/surge/tree/master
-每日打卡提醒（corn "0 9,18 * * 1-5" 周一到周五，早九晚六）+ 每日壹句（有道词典）+ 跳转钉钉打卡页面（下拉通知点击链接）
+每日蚂蚁收能量提醒（corn "11 7 * * *" 每天7:11）+ 每日壹句（有道词典）+ 跳转支付宝蚂蚁森林页面（下拉通知点击链接）
 */
 
 const $tool = new tool()
 $tool.get('https://dict.youdao.com/infoline/style/cardList?mode=publish&client=mobile&style=daily&size=2', function (error, response, data) {
     let obj = JSON.parse(data);
     let date = new Date();
-    let isAM = date.getHours() < 12 ? true : false
+    let isAM = date.getHours() < 12 ? true : false;
     let title = '每日' + (isAM ? ' 壹句' : ' 壹句') + (isAM ? ' ☀️' : ' 🌙');
     let subtitle = '';
-        let content = 'dingtalk://dingtalkclient/page/link?url=https://attend.dingtalk.com/attend/index.html';
+    let content = '收能量啦👉' + 'alipay://platformapi/startapp?appId=60000002';
     if (!error) {
         if (obj && obj.length > 1) {
             let yi = obj[1];
-            content = yi.title + '\n' + yi.summary ;
+            content = yi.title + '\n' + yi.summary + '\n\n' + content;
         }
     }
     $tool.notify(title, subtitle, content);
